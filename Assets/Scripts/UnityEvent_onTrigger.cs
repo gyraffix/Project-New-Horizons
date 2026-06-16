c3b66f5f69eef596c61599cb4f90ea68b3b19cfa
Ties Spit <tiesspit@gmail.com>
2026-06-16 15:20:09 +0200

    Changed trigger check to 2D

--- Assets/Scripts/UnityEvent_onTrigger.cs
+++ Assets/Scripts/UnityEvent_onTrigger.cs
@@ -3,19 +3,25 @@ using UnityEngine.Events;
 
 public class UnityEvent_onTrigger : MonoBehaviour
 {
+    [SerializeField] private string checkForTag;
+
     [SerializeField] private UnityEvent onTriggerEnter;
     [SerializeField] private UnityEvent onTriggerStay;
     [SerializeField] private UnityEvent onTriggerExit;
 
-    private void OnTriggerEnter(Collider other)
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
+        if (checkForTag == null || !other.CompareTag(checkForTag))
+            return;
+            
         onTriggerEnter.Invoke();
     }
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerStay2D(Collider2D other)
     {
         onTriggerStay.Invoke();
     }
-    private void OnTriggerExit(Collider other)
+    private void OnTriggerExit2D(Collider2D other)
     {
         onTriggerExit.Invoke();
     }
