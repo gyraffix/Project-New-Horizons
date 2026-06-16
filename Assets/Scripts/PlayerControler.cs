f71f5bd03635cd0bc30f42f42add64eb0902bcdc
Ties Spit <tiesspit@gmail.com>
2026-06-16 12:20:38 +0200

    Added PlayerControler

---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    [SerializeField] private KeyCode moveRight;
+    [SerializeField] private KeyCode moveLeft;
+    
+    [SerializeField] private float magnitude;
+
+    private Rigidbody2D rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NormalMovement();
+    }
+
+    private void NormalMovement()
+    {
+        Vector2 directions = new Vector2(1, -1);
+        float direction = 0;
+        if (Input.GetKey(moveRight))
+        {
+            direction = directions.x;
+        }
+        else if (Input.GetKey(moveLeft))
+        {
+            direction = directions.y;
+        }
+
+
+        rb.AddForce(new Vector2(magnitude, 0) * direction, ForceMode2D.Force);
+    }
+}
