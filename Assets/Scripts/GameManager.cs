c95cf1abaedb73dc19eb61728f77e1bace1c196e
Renzo Rodriguez <r.nrf@runbox.com>
2026-06-16 12:17:34 +0200

    Game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    [SerializeField] private List<Rigidbody2D> gravityAffectedObjects = new();
+    [SerializeField] private Level[] levels;
+
+
+    #region Public getters
+    public static GameManager Instance
+    { get { return instance; } }
+
+    #endregion
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+    public void AddGravityAffectedObj(Rigidbody2D gameObject)
+    {
+        gravityAffectedObjects.Add(gameObject);
+    }
+
+    public void AddGravityAffectedObj(List<Rigidbody2D> gameObjects)
+    {
+        foreach (Rigidbody2D gameObject in gameObjects)
+        {
+            gravityAffectedObjects.Add(gameObject);
+        }
+    }
+
+    public void LevelComplete(int levelNumber, int numberOfStars)
+    {
+        if (numberOfStars > levels[levelNumber].maxStarts)
+            levels[levelNumber].maxStarts = numberOfStars;
+    }
+}
+public class Level
+{
+    public string name;
+    public int maxStarts;
+}
