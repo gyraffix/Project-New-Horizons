70c1837dc86ff770eef2a96264a54a17e47c83d1
Ties Spit <tiesspit@gmail.com>
2026-06-19 11:13:43 +0200

    Better usability for rain

--- Assets/Scripts/RainManager.cs
+++ Assets/Scripts/RainManager.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RainManager : MonoBehaviour
@@ -8,7 +9,10 @@ public class RainManager : MonoBehaviour
     [SerializeField] private GameObject gravUp;
     [SerializeField] private GameObject hitboxes;
 
-    private bool covered;
+    [SerializeField] private ParticleSystem[] allRainDown;
+    [SerializeField] private ParticleSystem[] allRainUp;
+
+    private bool covered;    
 
     void Start()
     {
@@ -16,6 +20,9 @@ public class RainManager : MonoBehaviour
         gravDown = transform.Find("Grav Down").gameObject;
         gravUp = transform.Find("Grav Up").gameObject;
         hitboxes = transform.Find("Rain Hitboxes").gameObject;
+              
+        allRainDown = gravDown.transform.GetComponentsInChildren<ParticleSystem>(true);
+        allRainUp = gravUp.transform.GetComponentsInChildren<ParticleSystem>(true);                
     }
 
     void Update()
@@ -40,5 +47,19 @@ public class RainManager : MonoBehaviour
     public void SwitchCoverState()
     {
         covered = !covered;
+    }
+
+    private void OnDrawGizmos()
+    {
+        foreach (var partSystem in allRainDown)
+        {
+            Gizmos.DrawLine(new Vector2(partSystem.transform.position.x - (partSystem.shape.length / 2), partSystem.transform.position.y), 
+                new Vector2(partSystem.transform.position.x + (partSystem.shape.length / 2), partSystem.transform.position.y));            
+        }
+        foreach (var partSystem in allRainUp)
+        {
+            Gizmos.DrawLine(new Vector2(partSystem.transform.position.x - (partSystem.shape.length / 2), partSystem.transform.position.y),
+                new Vector2(partSystem.transform.position.x + (partSystem.shape.length / 2), partSystem.transform.position.y));            
+        }        
     }
 }
