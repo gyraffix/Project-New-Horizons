060d5b18ee32e520dd44d4de7ddf265886465db4
Renzo Rodriguez <r.nrf@runbox.com>
2026-06-25 14:53:46 +0200

    Added most sounds

--- Assets/Scripts/RandomSounds.cs
+++ Assets/Scripts/RandomSounds.cs
@@ -1,12 +1,15 @@
+using System;
 using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class RandomSounds : MonoBehaviour
 {
     private AudioSource audioSource;
-    [SerializeField] private AudioClip[] audioClips;
+    [SerializeField] private Sound[] sounds;
     [SerializeField] private Vector2 randomInterval;
-    [SerializeField, Range(0, 1)] private float[] volumes;
 
     private void Start()
     {
@@ -15,13 +18,35 @@ public class RandomSounds : MonoBehaviour
     }
     private IEnumerator PlaySounds()
     {
-        if (audioClips != null && volumes.Length == audioClips.Length)
-            while (true)
+        if (sounds == null)
+            yield break;
+        List<Sound> weightedSounds = new();
+        foreach (Sound sound in sounds)
+        {
+            for (int i = 0; i < sound.weight; i++)
             {
-                int index = Random.Range(0, audioClips.Length - 1);
-                audioSource.PlayOneShot(audioClips[index], volumes[index]);
-                yield return new WaitForSeconds(audioClips[index].length);
-                yield return new WaitForSeconds(Random.Range(randomInterval.x, randomInterval.y));
+                weightedSounds.Add(sound);
             }
+        }
+
+        while (true)
+        {
+            Sound sound = weightedSounds[Random.Range(0, weightedSounds.Count - 1)];
+
+            audioSource.clip = sound.audioClip;
+            audioSource.volume = sound.volume;
+            audioSource.Play();
+
+            yield return new WaitForSeconds(sound.audioClip.length);
+            yield return new WaitForSeconds(Random.Range(randomInterval.x, randomInterval.y));
+        }
     }
+}
+
+[Serializable]
+public class Sound
+{
+    public AudioClip audioClip;
+    [Range(0.0f, 1.0f)] public float volume;
+    [Min(0)] public int weight;
 }
