1d750c4222e7a8a149db687117fd67b727410e49
Dylan Valkenaar <valkdylan@gmail.com>
2026-06-30 14:56:16 +0200

    Thwomp

--- Assets/Scripts/DesignTools/AffectedByGravity.cs
+++ Assets/Scripts/DesignTools/AffectedByGravity.cs
@@ -19,24 +19,40 @@ public class AffectedByGravity : MonoBehaviour
         GameManager.Instance.AddGravityAffectedObj(GetComponent<Rigidbody2D>());
     }
 
+    private void Update()
+    {
+        if (gameObject.CompareTag("Player") && !CheckpointManager.Instance.Dead)
+        {
+            RaycastHit2D hitLeft = Physics2D.Raycast(transform.position + Vector3.left * 0.5f + Vector3.down * 0.6f, Vector2.up, 1.2f);
+            RaycastHit2D hitRight = Physics2D.Raycast(transform.position + Vector3.right * 0.5f + Vector3.down * 0.6f, Vector2.up, 1.2f);
+            RaycastHit2D hitUp = Physics2D.Raycast(transform.position + Vector3.left * 0.3f + Vector3.up * 0.45f, Vector3.right, 0.6f);
+            RaycastHit2D hitDown = Physics2D.Raycast(transform.position + Vector3.left * 0.3f - Vector3.up * 0.45f, Vector3.right, 0.6f);
+
+
+            Debug.DrawRay(transform.position + Vector3.left * 0.25f + Vector3.up * 0.5f, Vector3.right * 0.5f);
+
+            if (hitUp && hitDown && hitUp.collider.CompareTag("Ground") && hitDown.collider.CompareTag("Ground"))
+            {
+                CheckpointManager.Instance.Respawn();
+            }
+
+            if ((hitLeft && hitLeft.collider.CompareTag("Ground")) || (hitRight && hitRight.collider.CompareTag("Ground")))
+            {
+                inAir = false;
+
+                GameManager.Instance.ResetGravity();
+                GetComponent<Animator>().SetBool("Falling", false);
+
+            }
+
+            else StartCoroutine(CoyoteTime());
+        }
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
-        RaycastHit2D hitLeft = Physics2D.Raycast(transform.position + Vector3.left * 0.5f + Vector3.down * 0.6f, Vector2.up, 1.2f);
-        RaycastHit2D hitRight = Physics2D.Raycast(transform.position + Vector3.right * 0.5f + Vector3.down * 0.6f, Vector2.up, 1.2f);
-
-        Debug.DrawLine(transform.position + Vector3.left * 0.5f + Vector3.down * 0.7f, transform.position + Vector3.down * 0.6f, color: Color.blue);
-        if ((hitLeft && hitLeft.collider.CompareTag("Ground")) || (hitRight && hitRight.collider.CompareTag("Ground")))
-        {
-            inAir = false;
-            if (gameObject.CompareTag("Player"))
-            {
-                GameManager.Instance.ResetGravity();
-                GetComponent<Animator>().SetBool("Falling", false);
-            }
-        }
-        else StartCoroutine(CoyoteTime());
-
+        
     }
 
     IEnumerator CoyoteTime()
